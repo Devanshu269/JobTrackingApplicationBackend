defd4c71f681815920b227dedb56ca8ace13600d
Devanshu shekhar <devanshu.shekhar2@gmail.com>
2026-08-11 23:04:28 +0530

    Updated the Default Resume URL

--- src/main/java/com/jobtracker/Constants/AuthConstants.java
+++ src/main/java/com/jobtracker/Constants/AuthConstants.java
@@ -3,5 +3,5 @@ package com.jobtracker.Constants;
 public class AuthConstants {
     public static final String AUTH_HEADER = "Authorization";
     public static final String JWT_TOKEN_PREFIX = "Bearer ";
-    public static final String DEFAULT_AVATAR_URL = "https://cdn-icons-png.flaticon.com/512/149/149071.png";
+    public static final String DEFAULT_AVATAR_URL = "https://api.dicebear.com/9.x/bottts/svg?seed=Felix&backgroundColor=b6e3f4";
 }
